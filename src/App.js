a239e5cb137fe70b887963f1e0fb0625e9eb493b
Daewoong Lim <daewoong1025@gmail.com>
2024-05-29 17:06:27 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { animals } from "./animals";
+import React from "react";
+import { createRoot } from "react-dom/client";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // const container = document.getElementById('root');
+  // const root = createRoot(container);
+  
+  const title = "";
+  const animalFacts = <h1>{title === "" ? "Click an animal for a fun fact" : title}</h1>;
+  console.log(title === "" ? "Click an animal for a fun fact" : title);
+  
+  // root.render(animalFacts);
+  return animalFacts;
 }
 
 export default App;
