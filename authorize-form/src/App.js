8b5e5b41e194e7f68d0385ea7a0852fc8a804d01
Daewoong Lim <daewoong1025@gmail.com>
2024-05-30 17:08:10 +0900

    Finish Project
    Added authorization form (no actual data handling)

--- authorize-form/src/App.js
+++ authorize-form/src/App.js
@@ -2,21 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function handleSubmit() {
+    const id = document.getElementById('username').value;
+    const pw = document.getElementById('password').value;
+    if (id === "admin" && pw === "123") {
+      document.getElementById('message').innerHTML = "Correct User";
+    } else {
+      document.getElementById('message').innerHTML = "Incorrect User";
+    };
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" width="300"/>
+        <p>LOGIN</p>
+        <input type='text' id='username' placeholder='ID' />
+        <input type='password' id='password' placeholder='Password' />
+        <br />
+        <button onClick={handleSubmit}>Submit</button>
+        <p id='message'></p>
       </header>
     </div>
   );
